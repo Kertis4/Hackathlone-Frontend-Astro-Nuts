103972dfd25db4b04af8cff1c8c0cfb7d910292d
layli43 <layliuanyu@gmail.com>
2025-10-04 09:08:56 +0100

    eslint+prettier config for code format-on-save formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // Tailwind styles
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById("root")! as HTMLElement).render(
-  <App />
-);
+import App from './App';
+import './index.css'; // Tailwind styles
+
+ReactDOM.createRoot(document.getElementById('root')! as HTMLElement).render(<App />);
